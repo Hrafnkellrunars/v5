d53739fa5ac189cb4ac4c6c462d39c25e0a53269
Hrafnkellrunars <hrafnkellrunars@gmail.com>
2018-02-19 12:10:10 +0000

    Update index.py

--- index.py
+++ index.py
@@ -7,13 +7,13 @@ with urllib.request.urlopen("http://apis.is/concerts") as url:
 
 @route('/')
 def index():
-    return template('static/templates/index.tpl', data=gogn)
+    return template('index.tpl', data=gogn)
 
 run()
 
 
 @error(404)
 def error404(error):
-    return template('static/templates/error.tpl')
+    return template('error.tpl')
 
 run(host='0.0.0.0', port=os.environ.get('PORT'))
